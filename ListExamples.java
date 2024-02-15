db5aedb4096e8b5889f9b0f3e3167c0c7153353d
lotusrn <147683235+lotusrn@users.noreply.github.com>
2024-02-15 15:28:22 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-interface StringChecker { boolean checkString(String s); }
+interface StringChecked { boolean checkString(String s); }
 
 class ListExamples {
 
